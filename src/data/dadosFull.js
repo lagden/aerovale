d9244a3aa100be482e73c5e282fbc14b1a6b6a34
Thiago Lagden <lagden@gmail.com>
2013-05-15 19:23:42 -0300

    sem source e datas oks

--- src/data/dadosFull.js
+++ src/data/dadosFull.js
@@ -1,210 +1,274 @@
-[
-    {
-        "id": 1,
-        "nome": "Aerovale Sul (ERJ135LR)",
-        "voos": []
-    },
-    {
-        "id": 2,
-        "nome": "Aerovale Caravan",
-        "voos": []
-    },
-    {
-        "id": 3,
-        "nome": "Aerovale Norte (EMB-190)",
-        "voos": [
-            {
-                "id": 10,
-                "diadasemana": "Segunda-feira",
-                "horariopartida": "07:00",
-                "horariochegada": "07:50",
-                "origem": {
-                    "id": 5,
-                    "nome": "Belo Horizonte",
-                    "lat": 1231,
-                    "lon": 12312,
-                    "sigla": "BHZ",
-                    "aeroporto": {
-                        "id": 1,
-                        "nome": "Aeroporto de Carapicuiba",
-                        "endereco": "Rua Santa Marta",
-                        "observacao": "Obervação Teste"
-                    }
-                },
-                "destino": {
-                    "id": 6,
-                    "nome": "Rio de Janeiro",
-                    "lat": 123456,
-                    "lon": 123456,
-                    "sigla": "RIO",
-                    "aeroporto": {
-                        "id": 1,
-                        "nome": "Aeroporto de Carapicuiba",
-                        "endereco": "Rua Santa Marta",
-                        "observacao": "Obervação Teste"
-                    }
-                }
-            },
-            {
-                "id": 12,
-                "diadasemana": "Segunda-feira",
-                "horariopartida": "08:20",
-                "horariochegada": "09:10",
-                "origem": {
-                    "id": 6,
-                    "nome": "Rio de Janeiro",
-                    "lat": 123456,
-                    "lon": 123456,
-                    "sigla": "RIO",
-                    "aeroporto": {
-                        "id": 1,
-                        "nome": "Aeroporto de Carapicuiba",
-                        "endereco": "Rua Santa Marta",
-                        "observacao": "Obervação Teste"
-                    }
-                },
-                "destino": {
-                    "id": 5,
-                    "nome": "Belo Horizonte",
-                    "lat": 1231,
-                    "lon": 12312,
-                    "sigla": "BHZ",
-                    "aeroporto": {
-                        "id": 1,
-                        "nome": "Aeroporto de Carapicuiba",
-                        "endereco": "Rua Santa Marta",
-                        "observacao": "Obervação Teste"
-                    }
-                }
-            },
-            {
-                "id": 13,
-                "diadasemana": "Segunda-feira",
-                "horariopartida": "09:40",
-                "horariochegada": "12:00",
-                "origem": {
-                    "id": 5,
-                    "nome": "Belo Horizonte",
-                    "lat": 1231,
-                    "lon": 12312,
-                    "sigla": "BHZ",
-                    "aeroporto": {
-                        "id": 1,
-                        "nome": "Aeroporto de Carapicuiba",
-                        "endereco": "Rua Santa Marta",
-                        "observacao": "Obervação Teste"
-                    }
-                },
-                "destino": {
-                    "id": 7,
-                    "nome": "Carajás",
-                    "lat": 123456,
-                    "lon": 123456,
-                    "sigla": "CKS",
-                    "aeroporto": {
-                        "id": 1,
-                        "nome": "Aeroporto de Carapicuiba",
-                        "endereco": "Rua Santa Marta",
-                        "observacao": "Obervação Teste"
-                    }
-                }
-            },
-            {
-                "id": 14,
-                "diadasemana": "Segunda-feira",
-                "horariopartida": "17:00",
-                "horariochegada": "19:20",
-                "origem": {
-                    "id": 7,
-                    "nome": "Carajás",
-                    "lat": 123456,
-                    "lon": 123456,
-                    "sigla": "CKS",
-                    "aeroporto": {
-                        "id": 1,
-                        "nome": "Aeroporto de Carapicuiba",
-                        "endereco": "Rua Santa Marta",
-                        "observacao": "Obervação Teste"
-                    }
-                },
-                "destino": {
-                    "id": 5,
-                    "nome": "Belo Horizonte",
-                    "lat": 1231,
-                    "lon": 12312,
-                    "sigla": "BHZ",
-                    "aeroporto": {
-                        "id": 1,
-                        "nome": "Aeroporto de Carapicuiba",
-                        "endereco": "Rua Santa Marta",
-                        "observacao": "Obervação Teste"
-                    }
-                }
-            },
-            {
-                "id": 15,
-                "diadasemana": "Terça-feira",
-                "horariopartida": "07:00",
-                "horariochegada": "09:20",
-                "origem": {
-                    "id": 5,
-                    "nome": "Belo Horizonte",
-                    "lat": 1231,
-                    "lon": 12312,
-                    "sigla": "BHZ",
-                    "aeroporto": {
-                        "id": 1,
-                        "nome": "Aeroporto de Carapicuiba",
-                        "endereco": "Rua Santa Marta",
-                        "observacao": "Obervação Teste"
-                    }
-                },
-                "destino": {
-                    "id": 7,
-                    "nome": "Carajás",
-                    "lat": 123456,
-                    "lon": 123456,
-                    "sigla": "CKS",
-                    "aeroporto": {
-                        "id": 1,
-                        "nome": "Aeroporto de Carapicuiba",
-                        "endereco": "Rua Santa Marta",
-                        "observacao": "Obervação Teste"
-                    }
-                }
-            },
-            {
-                "id": 16,
-                "diadasemana": "Terça-feira",
-                "horariopartida": "17:00",
-                "horariochegada": "19:20",
-                "origem": {
-                    "id": 7,
-                    "nome": "Carajás",
-                    "lat": 123456,
-                    "lon": 123456,
-                    "sigla": "CKS",
-                    "aeroporto": {
-                        "id": 1,
-                        "nome": "Aeroporto de Carapicuiba",
-                        "endereco": "Rua Santa Marta",
-                        "observacao": "Obervação Teste"
-                    }
-                },
-                "destino": {
-                    "id": 5,
-                    "nome": "Belo Horizonte",
-                    "lat": 1231,
-                    "lon": 12312,
-                    "sigla": "BHZ",
-                    "aeroporto": {
-                        "id": 1,
-                        "nome": "Aeroporto de Carapicuiba",
-                        "endereco": "Rua Santa Marta",
-                        "observacao": "Obervação Teste"
-                    }
-                }
-            }
-        ]
-    }
-]
+[{
+    "aeronave": "Aerovale Norte (EMB-190)",
+    "diadasemana": "Segunda-feira",
+    "horariopartida": "07:00",
+    "horariochegada": "07:50",
+    "origem": "BHZ",
+    "destino": "RIO"
+}, {
+    "aeronave": "Aerovale Norte (EMB-190)",
+    "diadasemana": "Segunda-feira",
+    "horariopartida": "08:20",
+    "horariochegada": "09:10",
+    "origem": "RIO",
+    "destino": "BHZ"
+}, {
+    "aeronave": "Aerovale Norte (EMB-190)",
+    "diadasemana": "Segunda-feira",
+    "horariopartida": "09:40",
+    "horariochegada": "12:00",
+    "origem": "BHZ",
+    "destino": "CKS"
+}, {
+    "aeronave": "Aerovale Norte (EMB-190)",
+    "diadasemana": "Segunda-feira",
+    "horariopartida": "17:00",
+    "horariochegada": "19:20",
+    "origem": "CKS",
+    "destino": "BHZ"
+}, {
+    "aeronave": "Aerovale Norte (EMB-190)",
+    "diadasemana": "Terça-feira",
+    "horariopartida": "07:00",
+    "horariochegada": "09:20",
+    "origem": "BHZ",
+    "destino": "CKS"
+}, {
+    "aeronave": "Aerovale Norte (EMB-190)",
+    "diadasemana": "Terça-feira",
+    "horariopartida": "17:00",
+    "horariochegada": "19:20",
+    "origem": "CKS",
+    "destino": "BHZ"
+}, {
+    "aeronave": "Aerovale Norte (EMB-190)",
+    "diadasemana": "Sexta-feira",
+    "horariopartida": "07:00",
+    "horariochegada": "09:20",
+    "origem": "BHZ",
+    "destino": "CKS"
+}, {
+    "aeronave": "Aerovale Norte (EMB-190)",
+    "diadasemana": "Sexta-feira",
+    "horariopartida": "14:20",
+    "horariochegada": "16:40",
+    "origem": "CKS",
+    "destino": "BHZ"
+}, {
+    "aeronave": "Aerovale Norte (EMB-190)",
+    "diadasemana": "Sexta-feira",
+    "horariopartida": "17:10",
+    "horariochegada": "18:00",
+    "origem": "BHZ",
+    "destino": "RIO"
+}, {
+    "aeronave": "Aerovale Norte (EMB-190)",
+    "diadasemana": "Sexta-feira",
+    "horariopartida": "18:30",
+    "horariochegada": "19:20",
+    "origem": "RIO",
+    "destino": "BHZ"
+}, {
+    "aeronave": "Aerovale Sul (ERJ135LR)",
+    "diadasemana": "Terça-feira",
+    "horariopartida": "07:30",
+    "horariochegada": "08:30",
+    "origem": "BHZ",
+    "destino": "RIO"
+}, {
+    "aeronave": "Aerovale Sul (ERJ135LR)",
+    "diadasemana": "Terça-feira",
+    "horariopartida": "08:50",
+    "horariochegada": "09:50",
+    "origem": "RIO",
+    "destino": "BHZ"
+}, {
+    "aeronave": "Aerovale Sul (ERJ135LR)",
+    "diadasemana": "Terça-feira",
+    "horariopartida": "17:10",
+    "horariochegada": "18:10",
+    "origem": "BHZ",
+    "destino": "RIO"
+}, {
+    "aeronave": "Aerovale Sul (ERJ135LR)",
+    "diadasemana": "Terça-feira",
+    "horariopartida": "18:30",
+    "horariochegada": "19:30",
+    "origem": "RIO",
+    "destino": "BHZ"
+}, {
+    "aeronave": "Aerovale Sul (ERJ135LR)",
+    "diadasemana": "Quinta-feira",
+    "horariopartida": "07:30",
+    "horariochegada": "08:30",
+    "origem": "BHZ",
+    "destino": "RIO"
+}, {
+    "aeronave": "Aerovale Sul (ERJ135LR)",
+    "diadasemana": "Quinta-feira",
+    "horariopartida": "08:50",
+    "horariochegada": "09:50",
+    "origem": "RIO",
+    "destino": "BHZ"
+}, {
+    "aeronave": "Aerovale Sul (ERJ135LR)",
+    "diadasemana": "Quinta-feira",
+    "horariopartida": "17:10",
+    "horariochegada": "18:10",
+    "origem": "BHZ",
+    "destino": "RIO"
+}, {
+    "aeronave": "Aerovale Sul (ERJ135LR)",
+    "diadasemana": "Quinta-feira",
+    "horariopartida": "18:30",
+    "horariochegada": "19:30",
+    "origem": "RIO",
+    "destino": "BHZ"
+}, {
+    "aeronave": "Aerovale Caravan",
+    "diadasemana": "Segunda-feira",
+    "horariopartida": "12:30",
+    "horariochegada": "13:10",
+    "origem": "CKS",
+    "destino": "OIA"
+}, {
+    "aeronave": "Aerovale Caravan",
+    "diadasemana": "Segunda-feira",
+    "horariopartida": "13:20",
+    "horariochegada": "14:20",
+    "origem": "OIA",
+    "destino": "MAB"
+}, {
+    "aeronave": "Aerovale Caravan",
+    "diadasemana": "Segunda-feira",
+    "horariopartida": "14:30",
+    "horariochegada": "15:30",
+    "origem": "MAB",
+    "destino": "OIA"
+}, {
+    "aeronave": "Aerovale Caravan",
+    "diadasemana": "Segunda-feira",
+    "horariopartida": "15:40",
+    "horariochegada": "16:20",
+    "origem": "OIA",
+    "destino": "CKS"
+}, {
+    "aeronave": "Aerovale Caravan",
+    "diadasemana": "Terça-feira",
+    "horariopartida": "09:50",
+    "horariochegada": "10:30",
+    "origem": "CKS",
+    "destino": "OIA"
+}, {
+    "aeronave": "Aerovale Caravan",
+    "diadasemana": "Terça-feira",
+    "horariopartida": "10:40",
+    "horariochegada": "11:20",
+    "origem": "OIA",
+    "destino": "CKS"
+}, {
+    "aeronave": "Aerovale Caravan",
+    "diadasemana": "Quinta-feira",
+    "horariopartida": "09:50",
+    "horariochegada": "10:30",
+    "origem": "CKS",
+    "destino": "OIA"
+}, {
+    "aeronave": "Aerovale Caravan",
+    "diadasemana": "Quinta-feira",
+    "horariopartida": "10:40",
+    "horariochegada": "11:20",
+    "origem": "OIA",
+    "destino": "CKS"
+}, {
+    "aeronave": "Aerovale Caravan",
+    "diadasemana": "Sexta-feira",
+    "horariopartida": "08:30",
+    "horariochegada": "09:10",
+    "origem": "CKS",
+    "destino": "OIA"
+}, {
+    "aeronave": "Aerovale Caravan",
+    "diadasemana": "Sexta-feira",
+    "horariopartida": "09:20",
+    "horariochegada": "10:00",
+    "origem": "OIA",
+    "destino": "CKS"
+}, {
+    "aeronave": "Aerovale Caravan",
+    "diadasemana": "Sexta-feira",
+    "horariopartida": "10:10",
+    "horariochegada": "10:50",
+    "origem": "CKS",
+    "destino": "OIA"
+}, {
+    "aeronave": "Aerovale Caravan",
+    "diadasemana": "Sexta-feira",
+    "horariopartida": "11:00",
+    "horariochegada": "12:00",
+    "origem": "OIA",
+    "destino": "MAB"
+}, {
+    "aeronave": "Aerovale Caravan",
+    "diadasemana": "Sexta-feira",
+    "horariopartida": "12:10",
+    "horariochegada": "12:40",
+    "origem": "MAB",
+    "destino": "CKS"
+}, {
+    "aeronave": "Aerovale Norte (EMB-190)",
+    "diadasemana": "Sábado",
+    "horariopartida": "07:00",
+    "horariochegada": "09:20",
+    "origem": "BHZ",
+    "destino": "CKS"
+}, {
+    "aeronave": "Aerovale Norte (EMB-190)",
+    "diadasemana": "Sábado",
+    "horariopartida": "09:50",
+    "horariochegada": "12:10",
+    "origem": "CKS",
+    "destino": "BHZ"
+}, {
+    "aeronave": "Aerovale Norte (EMB-190)",
+    "diadasemana": "Domingo",
+    "horariopartida": "16:00",
+    "horariochegada": "18:20",
+    "origem": "BHZ",
+    "destino": "CKS"
+}, {
+    "aeronave": "Aerovale Norte (EMB-190)",
+    "diadasemana": "Domingo",
+    "horariopartida": "18:50",
+    "horariochegada": "21:10",
+    "origem": "CKS",
+    "destino": "BHZ"
+}, {
+    "aeronave": "Aerovale Caravan",
+    "diadasemana": "Sábado",
+    "horariopartida": "07:30",
+    "horariochegada": "08:10",
+    "origem": "CKS",
+    "destino": "OIA"
+}, {
+    "aeronave": "Aerovale Caravan",
+    "diadasemana": "Sábado",
+    "horariopartida": "08:20",
+    "horariochegada": "09:00",
+    "origem": "OIA",
+    "destino": "CKS"
+}, {
+    "aeronave": "Aerovale Caravan",
+    "diadasemana": "Domingo",
+    "horariopartida": "18:40",
+    "horariochegada": "19:20",
+    "origem": "CKS",
+    "destino": "OIA"
+}, {
+    "aeronave": "Aerovale Caravan",
+    "diadasemana": "Domingo",
+    "horariopartida": "19:30",
+    "horariochegada": "20:10",
+    "origem": "OIA",
+    "destino": "CKS"
+}]
